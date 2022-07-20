11c7c85b940d4f83e30d75d9922e6a70aa1bd7df
KimboSlice <kimberlyhoward529@gmail.com>
2022-07-19 22:14:19 -0400

    I attached user input to html page

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 
 // Define a method named promptUser
 const promptUser = () => {
-    console.log(fs.existsSync(  ));
-
     return inquirer.prompt([
         // Ask for name, employee ID, email, and office number of manager
         {
@@ -17,7 +15,7 @@ const promptUser = () => {
 
         {
             type: 'input',
-            name: 'manager_employee_ID',
+            name: 'manager_ID',
             message: "Enter the manager's employee ID."
         },
 
@@ -36,9 +34,121 @@ const promptUser = () => {
         // Ask user if they would like to add an engineer, intern, or finish adding
         {
             type: 'checkbox',
-            name: 'eng_int_finish',
+            name: 'eng_int_finish_1',
             message: 'Would you like to add an engineer or intern next? Select finish to skip this step and generate webpage.',
             choices: ['Add Engineer', 'Add Intern', 'Finish'],
+        },
+
+        // If engineer selected
+        {
+            type: 'input',
+            name: 'eng_name_1',
+            message: "Enter the engineer's name."
+        },
+
+        {
+            type: 'input',
+            name: 'eng_ID_1',
+            message: "Enter the engineer's ID",
+        },
+
+        {
+            type: 'input',
+            name: 'eng_email_1',
+            message: "Enter the engineer's email."
+        },
+
+        {
+            type: 'input',
+            name: 'eng_github_1',
+            message: "Enter the engineer's GitHub user name.",
+        },
+
+        {
+            type:"checkbox",
+            name:"eng_int_finish_2",
+            message:"Would you like to add another engineer, add an intern, or finish adding team memebers?",
+            choices: ["Add Engineer", "Add Intern", "Finish"]
+        },
+
+        {
+            type: 'input',
+            name: 'eng_name_2',
+            message: "Enter the engineer's name."
+        },
+
+        {
+            type: 'input',
+            name: 'eng_ID_2',
+            message: "Enter the engineer's ID",
+        },
+
+        {
+            type: 'input',
+            name: 'eng_email_2',
+            message: "Enter the engineer's email."
+        },
+
+        {
+            type: 'input',
+            name: 'eng_github_2',
+            message: "Enter the engineer's GitHub user name",
+        },
+
+        // If intern selected
+        {
+            type: 'input',
+            name: 'int_name_1',
+            message: "Enter the intern's name."
+        },
+
+        {
+            type: 'input',
+            name: 'int_ID_1',
+            message: "Enter the intern's ID",
+        },
+
+        {
+            type: 'input',
+            name: 'int_email_1',
+            message: "Enter the intern's email."
+        },
+
+        {
+            type: 'input',
+            name: 'int_school_1',
+            message: "Enter the intern's school",
+        },
+
+        {
+            type:"checkbox",
+            name:"eng_int_finish_3",
+            message:"Would you like to add another engineer, add an intern, or finish adding team memebers?",
+            choices: ["Add Engineer", "Add Intern", "Finish"]
+        },
+
+        {
+            type: 'input',
+            name: 'int_name_2',
+            message: "Enter the intern's name."
+        },
+
+        {
+            type: 'input',
+            name: 'int_ID_2',
+            message: "Enter the intern's ID",
+        },
+
+        {
+            type: 'input',
+            name: 'int_email_2',
+            message: "Enter the intern's email."
+        },
+
+        {
+            type: 'input',
+            name: 'int_school_2',
+            message: "Enter the intern's school",
         },
 
         // Give option to exit and generate webpage
@@ -51,92 +161,94 @@ const promptUser = () => {
     ]);
     }
 
-    // const engineer = `${}`
+    // const engineer = `${eng_int_finish}`
     // If engineer is chosen
-    // if() {}
-            // Ask for name, ID, email, and GitHub username 
+    // if(typeof `${eng_int_finish}` === 'string') {
+    //     console.log('user input defined!');
+    // }
+            //Ask for name, ID, email, and GitHub username 
 
-    // If intern is chosen
-            // Ask for name, ID, email, and school
+   // If intern is chosen
+         //   Ask for name, ID, email, and school
 
-    const generateHTML = ({}) =>
-  `<!DOCTYPE html>
+    const generateHTML = ({manager_name, manager_email, manager_ID, manager_office_num, eng_name_1, eng_ID_1, eng_email_1, eng_github_1, eng_name_2, eng_ID_2, eng_email_2, eng_github_2, int_name_1, int_name_2, int_ID_1, int_ID_2, int_email_1, int_email_2, int_school_1, int_school_2 }) => {
+    return `<!DOCTYPE html>
     <html lang="en">
     <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profile Generator</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
     <header class="d-flex justify-content-center bg-danger">
-            <h1 id='header-title' style='font-size: 30px' class="text-white m-4">My Team</h1>
-        </header>
+        <h1 id='header-title' style='font-size: 30px' class="text-white m-4">My Team</h1>
+    </header>
 
-        <section id="card-container" class="d-flex justify-content-center flex-wrap align-items-center">
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header"></div>
-                <div id="job-title"></div>
+        <section id="card-container" class="d-flex justify-content-center flex-wrap align-items-stretch mt-5 flex-wrap align-items-center">
+            <div class="card text-white bg-primary m-3" style="width: 18rem;">
+                <div class="card-header">${manager_name}</div>
+                <div id="job-title">Manager</div>
                 <div class="card-body bg-light">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item rounded">Cras justo odio</li>
+                    <ul class="list-group list-group-flush text-dark">
+                        <p class="list-group-item rounded">ID: ${manager_ID}</p>
                         <!-- Link to TO email box with email inside -->
-                        <li class="list-group-item rounded">Dapibus ac facilisis in</li>
-                        <li class="list-group-item rounded">Vestibulum at eros</li>
+                        <li class="list-group-item rounded">email: ${manager_email}</li>
+                        <li class="list-group-item rounded">office number: ${manager_office_num}</li>
                     </ul>
                 </div>
             </div> 
             
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header"></div>
-                <div id="job-title"></div>
+            <div class="card text-white bg-primary m-3" style="width: 18rem;">
+                <div class="card-header">${eng_name_1}</div>
+                <div id="job-title">Engineer</div>
                 <div class="card-body bg-light">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item rounded">Cras justo odio</li>
+                    <ul class="list-group list-group-flush text-dark">
+                        <li class="list-group-item rounded">ID: ${eng_ID_1}</li>
                         <!-- Link to TO email box with email inside -->
-                        <li class="list-group-item rounded">Dapibus ac facilisis in</li>
-                        <li class="list-group-item rounded">Vestibulum at eros</li>
+                        <li class="list-group-item rounded">Email: ${eng_email_1}</li>
+                        <li class="list-group-item rounded">GitHub: <link hred="#">www.github.com/${eng_github_1}</link></li>
                     </ul>
                 </div>
             </div>
 
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header"></div>
-                <div id="job-title"></div>
+            <div class="card text-white bg-primary m-3" style="width: 18rem;">
+                <div class="card-header">${eng_name_2}</div>
+                <div id="job-title">Engineer</div>
                 <div class="card-body bg-light">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item rounded">Cras justo odio</li>
+                    <ul class="list-group list-group-flush text-dark">
+                        <li class="list-group-item rounded">ID: ${eng_ID_2}</li>
                         <!-- Link to TO email box with email inside -->
-                        <li class="list-group-item rounded">Dapibus ac facilisis in</li>
-                        <li class="list-group-item rounded">Vestibulum at eros</li>
+                        <li class="list-group-item rounded">Email: ${eng_email_2}</li>
+                        <li class="list-group-item rounded">GitHub: <link hred="#">www.github.com/${eng_github_2}</link></li>
                     </ul>
                 </div>
             </div>
 
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header"></div>
-                <div id="job-title"></div>
+            <div class="card text-white bg-primary m-3" style="width: 18rem;">
+                <div class="card-header">${int_name_1}</div>
+                <div id="job-title">Intern</div>
                 <div class="card-body bg-light">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item rounded">Cras justo odio</li>
+                    <ul class="list-group list-group-flush text-dark">
+                        <li class="list-group-item rounded">ID: ${int_ID_1}</li>
                         <!-- Link to TO email box with email inside -->
-                        <li class="list-group-item rounded">Dapibus ac facilisis in</li>
-                        <li class="list-group-item rounded">Vestibulum at eros</li>
+                        <li class="list-group-item rounded">Email: ${int_email_1}</li>
+                        <li class="list-group-item rounded">School: ${int_school_1}</li>
                     </ul>
                 </div>
             </div>
 
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header"></div>
-                <div id="job-title"></div>
+            <div class="card text-white bg-primary m-3" style="width: 18rem;">
+                <div class="card-header">${int_name_2}</div>
+                <div id="job-title">Intern</div>
                 <div class="card-body bg-light">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item rounded">Cras justo odio</li>
+                    <ul class="list-group list-group-flush text-dark">
+                        <li class="list-group-item rounded">ID: ${int_ID_2}</li>
                         <!-- Link to TO email box with email inside -->
-                        <li class="list-group-item rounded">Dapibus ac facilisis in</li>
-                        <li class="list-group-item rounded">Vestibulum at eros</li>
+                        <li class="list-group-item rounded">Email: ${int_email_2}</li>
+                        <li class="list-group-item rounded">School: ${int_school_2}</li>
                     </ul>
                 </div>
             </div>
@@ -144,19 +256,17 @@ const promptUser = () => {
         <script src="./index.js"></script>
     </body>
     </html>`;
+    }
 
     // Use fs to prompt user when application runs
     const init = () => {
         promptUser()
-        .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+        .then((answers) => fs.writeFileSync('./dist/index.html', generateHTML(answers)))
+        .then(() => console.log('Successfully wrote to html file!'))
         .then((err) => console.error(err));
     };
 
-    init()
-
-    console.log(`${finish}`);
-
-    // console.log(`${eng_int_finish}`);
+    init();
 
     // Attach each input username to each github link
 
